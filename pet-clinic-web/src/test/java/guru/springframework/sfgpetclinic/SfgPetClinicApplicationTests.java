83ef2b05a5da2228b6f9a3f1d670d56681ae065d
mnixdorf <35327832+mnixdorf@users.noreply.github.com>
2023-01-02 11:12:53 +0100

    refactoring multimodule multimodel maven project. closes #1

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -2,9 +2,11 @@ package guru.springframework.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 
 @SpringBootTest
-class SfgPetClinicApplicationTests {
+public class SfgPetClinicApplicationTests {
 
 	@Test
 	void contextLoads() {
